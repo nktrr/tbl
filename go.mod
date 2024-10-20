f160f3752ad455824033d171aede090957cb6899
nktrr <nekitrudnevv@gmail.com>
2024-10-20 18:00:13 +0300

    init

---
+++ go.mod
@@ -0,0 +1,25 @@
+module tbl
+
+go 1.23.1
+
+require (
+	github.com/gookit/slog v0.5.6
+	github.com/phuslu/log v1.0.112
+	github.com/rs/zerolog v1.33.0
+	github.com/sirupsen/logrus v1.9.3
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/goutil v0.6.15 // indirect
+	github.com/gookit/gsr v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ tests/slog_perfomance_test.go
@@ -0,0 +1,179 @@
+package tests
+
+import (
+	"io"
+	"testing"
+
+	"github.com/gookit/slog"
+	"github.com/gookit/slog/handler"
+	phuslu "github.com/phuslu/log"
+	"github.com/rs/zerolog"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// In _example/ dir, run:
+//
+//	go test -v -cpu=4 -run=none -bench=. -benchtime=10s -benchmem bench_loglibs_test.go
+//
+// code refer:
+//
+//	https://github.com/phuslu/log
+var msg = "The quick brown fox jumps over the lazy dog"
+
+func BenchmarkZapNegative(b *testing.B) {
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(io.Discard),
+		zapcore.InfoLevel,
+	))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info(msg, zap.String("rate", "15"), zap.Int("low", 16), zap.Float32("high", 123.2))
+	}
+}
+
+func BenchmarkZapSugarNegative(b *testing.B) {
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(io.Discard),
+		// zapcore.AddSync(os.Stdout),
+		zapcore.InfoLevel,
+	)).Sugar()
+
+	// logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	// return
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	}
+}
+
+func BenchmarkZeroLogNegative(b *testing.B) {
+	logger := zerolog.New(io.Discard).With().Timestamp().Logger().Level(zerolog.InfoLevel)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info().Str("rate", "15").Int("low", 16).Float32("high", 123.2).Msg(msg)
+	}
+}
+
+func BenchmarkPhusLogNegative(b *testing.B) {
+	logger := phuslu.Logger{Level: phuslu.InfoLevel, Writer: phuslu.IOWriter{Writer: io.Discard}}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info().Str("rate", "15").Int("low", 16).Float32("high", 123.2).Msg(msg)
+	}
+}
+
+// "github.com/sirupsen/logrus"
+func BenchmarkLogrusNegative(b *testing.B) {
+	logger := logrus.New()
+	logger.Out = io.Discard
+	logger.Level = logrus.InfoLevel
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	}
+}
+
+func BenchmarkGookitSlogNegative(b *testing.B) {
+	logger := slog.NewWithHandlers(
+		handler.NewIOWriter(io.Discard, []slog.Level{slog.InfoLevel}),
+		// handler.NewIOWriter(os.Stdout, []slog.Level{slog.InfoLevel}),
+	)
+	logger.ReportCaller = false
+
+	// logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	// return
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	}
+}
+
+func BenchmarkZapPositive(b *testing.B) {
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(io.Discard),
+		zapcore.InfoLevel,
+	))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info(msg, zap.String("rate", "15"), zap.Int("low", 16), zap.Float32("high", 123.2))
+	}
+}
+
+func BenchmarkZapSugarPositive(b *testing.B) {
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(io.Discard),
+		zapcore.InfoLevel,
+	)).Sugar()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info(msg, zap.String("rate", "15"), zap.Int("low", 16), zap.Float32("high", 123.2))
+	}
+}
+
+func BenchmarkZeroLogPositive(b *testing.B) {
+	logger := zerolog.New(io.Discard).With().Timestamp().Logger().Level(zerolog.InfoLevel)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info().Str("rate", "15").Int("low", 16).Float32("high", 123.2).Msg(msg)
+	}
+}
+
+func BenchmarkPhusLogPositive(b *testing.B) {
+	logger := phuslu.Logger{Level: phuslu.InfoLevel, Writer: phuslu.IOWriter{Writer: io.Discard}}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info().Str("rate", "15").Int("low", 16).Float32("high", 123.2).Msg(msg)
+	}
+}
+
+func BenchmarkLogrusPositive(b *testing.B) {
+	logger := logrus.New()
+	logger.Out = io.Discard
+	logger.Level = logrus.InfoLevel
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	}
+}
+
+func BenchmarkGookitSlogPositive(b *testing.B) {
+	logger := slog.NewWithHandlers(
+		handler.NewIOWriter(io.Discard, []slog.Level{slog.InfoLevel}),
+	)
+	logger.ReportCaller = false
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("rate", "15", "low", 16, "high", 123.2, msg)
+	}
+}
